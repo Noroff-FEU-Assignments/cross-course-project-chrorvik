fbec0443bfd3c72da01476c688d0cf18d5799a71
Chris <dmlp20yv1@mozmail.com>
2023-09-24 12:15:28 +0200

    added images from API

--- js/populateUI.js
+++ js/populateUI.js
@@ -5,15 +5,22 @@ export function generateProductCards(products){
     products.forEach((product) => {
         const li = document.createElement('li');
 
+        // Splitte tittelen ved de to første mellomrommene
+
+    const [firstWord, secondWord, ...remainingWords] = product.title.split(' ');
+    const mainTitle = `${firstWord} ${secondWord}`;
+    const subtitle = remainingWords.join(' ');
+
         li.innerHTML = `
             <a href="#" class="card card-${ul.children.length + 1}">
-            <div class="product-placeholder">
+            <div class="product-placeholder" style="background-image: url('${product.image}');">
                 <div class="product-btn cta-buttons">More info</div>
             </div>
             <div class="card-fav"></div>
-            <h2>${product.title}</h2>
-            <h3>${product.description}</h3>
-            <p>${product.price} NOK</p>
+            <h2>${mainTitle}</h2> <!-- Hovedtittel -->
+            <h3>${subtitle}</h3> <!-- Undertittel -->
+            <p>${product.description}</p>
+            <p class="price">$${product.price}</p>
             </a>
         `;
         
