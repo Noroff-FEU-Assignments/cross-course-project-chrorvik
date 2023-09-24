7051330d7980f21fad63f01067dd0f7cf1cc9deb
Chris <dmlp20yv1@mozmail.com>
2023-09-24 18:48:36 +0200

    added popup and run autoformat documents

--- js/populateUI.js
+++ js/populateUI.js
@@ -1,19 +1,21 @@
-export function generateProductCards(products){
-    const ul = document.querySelector('.card-container');
-    ul.innerHTML = ''; // Tømmer listen hvis den allerede har elementer
+export function generateProductCards(products) {
+  const ul = document.querySelector(".card-container");
+  ul.innerHTML = ""; // Tømmer listen hvis den allerede har elementer
 
-    products.forEach((product) => {
-        const li = document.createElement('li');
+  products.forEach((product) => {
+    const li = document.createElement("li");
 
-        // Splitte tittelen ved de to første mellomrommene
+    // Splitte tittelen ved de to første mellomrommene
 
-    const [firstWord, secondWord, ...remainingWords] = product.title.split(' ');
+    const [firstWord, secondWord, ...remainingWords] = product.title.split(" ");
     const mainTitle = `${firstWord} ${secondWord}`;
-    const subtitle = remainingWords.join(' ');
+    const subtitle = remainingWords.join(" ");
 
-        li.innerHTML = `
+    li.innerHTML = `
             <a href="#" class="card card-${ul.children.length + 1}">
-            <div class="product-placeholder" style="background-image: url('${product.image}');">
+            <div class="product-placeholder" style="background-image: url('${
+              product.image
+            }');">
                 <div class="product-btn cta-buttons">More info</div>
             </div>
             <div class="card-fav"></div>
@@ -23,7 +25,23 @@ export function generateProductCards(products){
             <p class="price">$${product.price}</p>
             </a>
         `;
-        
-        ul.appendChild(li);
+    // Legger til klikkhendelse på 'More info'-knappen
+    li.querySelector(".product-btn").addEventListener("click", function () {
+      // Åpner popupen
+      document.getElementById("productPopup").style.display = "block";
+
+      // Setter inn produktinfo i popupen
+      document.getElementById("popupTitle").innerText = product.title;
+      document.getElementById("popupImage").src = product.image;
+      document.getElementById("popupDescription").innerText =
+        product.description;
+      document.getElementById("popupPrice").innerText = "$" + product.price;
     });
+
+    ul.appendChild(li);
+  });
+  // Lukker popupen når brukeren klikker på 'close'-knappen
+  document.querySelector(".close").addEventListener("click", function () {
+    document.getElementById("productPopup").style.display = "none";
+  });
 }
